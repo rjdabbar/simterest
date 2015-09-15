1a38e3f66045503ac56a593e47cd8bda8d7caa6b
RJ Dabbar <rjdabbar@gmail.com>
2015-09-15 09:53:50 -0400

    Add actions for session control and routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,20 @@
 class SessionsController < ApplicationController
 
   def new
-
   end
 
   def create
-
+    @user = User.find_by_credentials(user_params[:username], user_params[:password])
+    if @user
+      sign_in!(@user)
+      redirect_to root
+    else
+      render :new
+    end
   end
 
   def destroy
-
+    sign_out!
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,45 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
+    render :new
   end
 
   def create
+    @user = User.new
+    if @user.save(user_params)
 
+    else
+      render :new
+    end
   end
 
   def show
-
+    @user = User.find(params[:id])
+    render :show
   end
 
   def index
-
+    @users = User.all
+    render :index
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
 
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @user = User.find(params[:id])
+    @user.destroy!
+    redirect_to new_users_url
   end
 end
