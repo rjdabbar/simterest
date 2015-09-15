39d54c1b14f6c56416a77d180a55480686ccbbd6
RJ Dabbar <rjdabbar@gmail.com>
2015-09-15 09:47:40 -0400

    Create Users and Sessions Controllers and add helper methods to Application Controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
