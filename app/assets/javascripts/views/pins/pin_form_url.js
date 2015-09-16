f8f8ead1e52046f6ad1d06bab6e1ccafb9084bfe
RJ Dabbar <rjdabbar@gmail.com>
2015-09-16 12:29:20 -0400

    Source url being passed along with model bbetween modals

--- app/assets/javascripts/views/pins/pin_form_url.js
+++ app/assets/javascripts/views/pins/pin_form_url.js
@@ -1,10 +1,27 @@
 Simterest.Views.PinFormUrl = Backbone.View.extend({
   template: JST["pins/pin_form_url"],
 
+  events: {
+    "click button.next": "openForm"
+  },
+
   initialize: function () {},
 
   render: function () {
     this.$el.html(this.template());
     return this;
+  },
+
+  openForm: function (e) {
+    e.preventDefault();
+
+    var data = $("input").serializeJSON();
+    this.model.set(data.pin)
+
+    var view = new Simterest.Views.PinForm({
+      model: this.model,
+      collection: this.collection
+    });
+    this.collection.trigger("openModal", view)
   }
 })
