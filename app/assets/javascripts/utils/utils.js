4441a1479f9ae74c2099a6497a36cb5b0a501a41
RJ Dabbar <rjdabbar@gmail.com>
2015-09-17 14:40:12 -0400

    add saveFormData prototype to models

--- app/assets/javascripts/utils/utils.js
+++ app/assets/javascripts/utils/utils.js
@@ -16,6 +16,27 @@ Backbone.Collection.prototype.getOrFetch = function (id) {
   return model;
 };
 
+Backbone.Models.prototype.saveFormData = function (formData, options) {
+  var method = this.isNew() ? "POST" : "PUT";
+  var model = this;
+
+  $.ajax({
+    url: _.result(model, "url"),
+    type: method,
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function(resp){
+      model.set(model.parse(resp));
+      model.trigger('sync', model, resp, options);
+      options.success && options.success(model, resp, options);
+    },
+    error: function(resp){
+      options.error && options.error(model, resp, options);
+    }
+  });
+}
+
 Backbone.View.prototype.setModal = function (view) {
   this._modal = view;
 };
