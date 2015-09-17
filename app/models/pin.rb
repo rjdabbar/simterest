6be870df75bf4df0ebed0d15651c02ac65c7f435
RJ Dabbar <rjdabbar@gmail.com>
2015-09-17 15:33:49 -0400

    add image to pin model level

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,17 @@
 class Pin < ActiveRecord::Base
 
   validates :pinner_id, :board_id, presence: true
+  # validate :has_an_image_source
 
   belongs_to :pinner, foreign_key: :pinner_id, class_name: "User"
   belongs_to :via_user, foreign_key: :via_user_id, class_name: "User"
   belongs_to :board
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  # private
+
+  # def has_an_image_source
+  #   errors.add(:image, "requires an image") unless self.source_url || self.image
+  # end
 
 end
