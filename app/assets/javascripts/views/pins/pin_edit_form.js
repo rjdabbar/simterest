f439ac998ab62bac07e4dc04f92aa7bb5ead7239
RJ Dabbar <rjdabbar@gmail.com>
2015-09-17 09:22:54 -0400

    Add pin edit form view and template, untested

---
+++ app/assets/javascripts/views/pins/pin_edit_form.js
@@ -0,0 +1,40 @@
+Simterest.Views.PinEditForm = Backbone.View.extend({
+  template: JST["pins/pin_edit_form"],
+
+  events: {
+    "click button.save": "submit",
+    "click button.delete": "destroyPin",
+    "click button.cancel": "closeModal",
+    "click button.close": "closeModal"
+  },
+
+  initialize: function () {
+    this.listenTo(this.boards, "sync", this.render);
+    this.listenTo(this.model, "sync", this.render);
+    this.boards = new Simterest.Collections.Boards();
+    this.boards.fetch();
+  },
+
+  render: function () {
+    this.$el.html(this.template({
+      pin: this.model,
+      boards: this.boards
+    }));
+    return this;
+  },
+
+  submit: function (e) {
+    e.preventDefault();
+  },
+
+  destroyPin: function (e) {
+    e.preventDefault();
+    this.model.destroy();
+    this.collection.trigger("closeModal");
+  },
+
+  closeModal: function (e) {
+    e.preventDefault();
+    this.collection.trigger("closeModal");
+  }
+})
