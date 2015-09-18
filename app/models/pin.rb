c9c682a895a9392d13a1eb2cad08f7c2d1803265
RJ Dabbar <rjdabbar@gmail.com>
2015-09-18 09:09:46 -0400

    updated jbuilder for attached images, and set pin index item to display them

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -8,10 +8,15 @@ class Pin < ActiveRecord::Base
   belongs_to :board
   has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  after_initialize :set_image_url_to_source_url
   # private
 
   # def has_an_image_source
   #   errors.add(:image, "requires an image") unless self.source_url || self.image
   # end
 
+  def set_image_url_to_source_url
+    self.image_url ||= self.source_url
+  end
 end
--- app/views/api/boards/show.json.jbuilder
+++ app/views/api/boards/show.json.jbuilder
@@ -3,4 +3,5 @@ json.extract! @board, :id, :title, :description, :category,
 json.pins @board.pins do |pin|
   json.extract! pin, :id, :pinner_id, :description, :image_url, :board_id,
             :image_alt, :source_url, :via_user_id, :created_at, :updated_at
+  json.image asset_path(pin.image.url)
 end
