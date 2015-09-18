ab84c651227350003f8afad3d6e4d65beed4882a
RJ Dabbar <rjdabbar@gmail.com>
2015-09-18 17:47:29 -0400

    commit before revert

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::UsersController < ApplicationController
-  
+
   def new
     @user = User.new
     render :new
@@ -9,7 +9,6 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in!(@user)
-      redirect_to root_url
     else
       render :new
     end
