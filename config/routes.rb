277512a4403fab030e6b947525354ea79380b05a
RJ Dabbar <rjdabbar@gmail.com>
2015-09-18 14:52:37 -0400

    add api/session/show route and jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   namespace :api, defaults: { format: :json } do
-    resource :session, only: [:new, :create, :destroy]
+    resource :session, only: [:new, :create, :destroy, :show]
     resources :users
     resources :boards, except: [:edit]
     resources :pins
