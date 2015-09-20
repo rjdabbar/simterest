cc7254c1726aa64fe168087b9ba8c52a2ed00bf0
RJ Dabbar <rjdabbar@gmail.com>
2015-09-20 13:17:21 -0400

    add commented out code for an attempt at a belongs to association for pin -> board

--- app/assets/javascripts/models/pin.js
+++ app/assets/javascripts/models/pin.js
@@ -7,5 +7,12 @@ Simterest.Models.Pin = Backbone.Model.extend({
 
   toJSON: function () {
     return { pin: _.clone(this.attributes) }
-  }
+  },
+
+  // board: function () {
+  //   if (typeof this._board === "undefined") {
+  //     this._board = new Simterest.Models.Board();
+  //   }
+  //   return this._board
+  // }
 })
