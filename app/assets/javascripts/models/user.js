7b010fad13d2b79b443230415951d31b773d502a
RJ Dabbar <rjdabbar@gmail.com>
2015-09-20 12:38:55 -0400

    add current user model to backbone

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -30,3 +30,47 @@ Simterest.Models.User = Backbone.Model.extend({
 
   }
 })
+
+Simterest.Models.CurrentUser = Simterest.Models.User.extend({
+  url: "api/session",
+
+  isSignedIn: function () {
+    return !this.isNew();
+  },
+
+  signIn: function(options){
+  var model = this;
+  var credentials = {
+    "user[email]": options.email,
+    "user[password]": options.password
+  };
+
+  $.ajax({
+      url: this.url,
+      type: "POST",
+      data: credentials,
+      dataType: "json",
+      success: function(data){
+        model.set(data);
+        options.success && options.success();
+      },
+      error: function(){
+        options.error && options.error();
+      }
+    });
+  },
+
+  signOut: function(options){
+    var model = this;
+
+    $.ajax({
+      url: this.url,
+      type: "DELETE",
+      dataType: "json",
+      success: function(data){
+        model.clear();
+        options.success && options.success();
+      }
+    });
+  },
+})
