2d7f4fe47bae7d1a893855c6a070fc2c17936eea
RJ Dabbar <rjdabbar@gmail.com>
2015-09-22 11:22:31 -0400

    add search views files and searchResults collection

---
+++ app/assets/javascripts/collections/search_results.js
@@ -0,0 +1,13 @@
+Simterest.Collections.SearchRestults = Backbone.Collection.extend({
+  url: "api/search",
+
+  parse: function (resp) {
+    return resp;
+  },
+
+  model: function (attrs) {
+    var type = attrs._type;
+    delete attrs._type;
+    return new Simterest.Models[type](attrs);
+  }
+})
