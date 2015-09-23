9809efc16cdd11c91b15620595400e91c0420314
RJ Dabbar <rjdabbar@gmail.com>
2015-09-23 09:32:26 -0400

    added comment model and associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ActiveRecord::Base
+
+  validates :author_id, :pin_id, :body, presence: true
+  belongs_to :author, foreign_key: :author_id, class_name: "User"
+  belongs_to :pin
+  
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,21 +1,17 @@
 class Pin < ActiveRecord::Base
   include PgSearch
   validates :pinner_id, :board_id, presence: true
-  # validate :has_an_image_source
 
   belongs_to :pinner, foreign_key: :pinner_id, class_name: "User"
   belongs_to :via_user, foreign_key: :via_user_id, class_name: "User"
   belongs_to :board
+
+  has_many :comments
+  
   has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   multisearchable against: :description
-
-  # private
-
-  # def has_an_image_source
-  #   errors.add(:image, "requires an image") unless self.source_url || self.image
-  # end
 
   def get_image_url
     source_url || image.url
