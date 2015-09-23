e87d051322a03eeecd896dbda7b5d90c9fb2f58b
RJ Dabbar <rjdabbar@gmail.com>
2015-09-23 16:06:12 -0400

    add omniauth action to api::session controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -13,5 +13,15 @@ class Api::SessionsController < ApplicationController
     render json: {}
   end
 
+  def omniauth
+    user = User.find_or_create_by_auth_hash(auth_hash)
+    sign_in!(user)
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
 
 end
